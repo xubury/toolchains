ad3e50b593c6cec398e8a13e2c4794ef98c68a96
xubury <xubury97@gmail.com>
2022-02-20 16:35:21 +0800

    * update toolchain

--- windows-mingw-clang.cmake
+++ windows-mingw-clang.cmake
@@ -7,8 +7,6 @@ set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}clang++.exe)
 set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_PREFIX}gfortran.exe)
 set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}windres.exe)
 
-# target environment on the build host system
-set(CMAKE_FIND_ROOT_PATH D:/msys64/mingw64)
 
 # modify default behavior of FIND_XXX() commands
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
